ae885f292f8f2ae6554961c265711bcfbcc6f27b
tanyavohra <vohratanya5@gmail.com>
2026-03-20 19:25:33 +0530

    analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { Analytics } from "@vercel/analytics/react";
 createRoot(document.getElementById("root")).render(<App />);
